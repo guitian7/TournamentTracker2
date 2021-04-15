5dcde74a5e256f193f041693e4766dd12bb55638
Analia <analia.guitian7@gmail.com>
2021-04-15 03:43:02 -0300

    Conn

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List <IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles){
+            if (database){
+                //TODO- create the sql connection
+            }
+            else { 
+                //TODO- create the text connection
+            }
+        }
+    }
+}
